1f00137d5b426ce8383eacb4248354666933365a
keshav jha <keshav@keshavs-MacBook-Air.local>
2022-12-08 23:23:13 +0530

    day oen

---
+++ lua/keshav/core/highlights.lua
@@ -0,0 +1,11 @@
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.winblend = 0
+vim.opt.wildoptions = 'pum'
+vim.opt.pumblend = 5
+vim.opt.background = 'dark'
+
+-- Append charecters
+--vim.opt.list = true
+--vim.opt.listchars:append "space:⋅"
+--vim.opt.listchars:append "eol:↴"
